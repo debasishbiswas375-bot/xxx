6ea9db1f86ea24de68bb14f4f9c1cafa1ed9fd9a
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-13 19:09:57 +0530

    Update models.py

--- converter/models.py
+++ converter/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
 
-# Added for AccountingCoach style structure
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, blank=True)
@@ -34,4 +33,12 @@ class Topic(models.Model):
     def __str__(self):
         return self.title
 
-# Keep your existing Product and ConversionLog models below this...
+# Keep your existing Product and ConversionLog models here as well
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
